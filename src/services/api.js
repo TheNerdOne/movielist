9f40a0192d20be4ea638a9c87707d7fcd30b0969
thenerdone <poria9chegeni@gmail.com>
2022-01-20 21:10:29 +0330

    add api configs

---
+++ src/services/api.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+import { API_URL } from '../config/HTTP'
+const key = 'api_key=f62f750b70a8ef11dad44670cfb6aa57'
+const Api = {
+
+    init() {
+        axios.defaults.baseURL = API_URL
+    },
+
+    query(resource, params) {
+        return axios.get(`${resource}?${key}`, params)
+    },
+
+    get(resource) {
+        return axios.get(`${resource}?${key}`)
+    },
+
+    post(resource, params) {
+        return axios.post(`${resource}?${key}`, params)
+    },
+}
+
+export default Api
