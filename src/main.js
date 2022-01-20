ff63fb6a41afba22bea980bacda45572d908cf1e
thenerdone <poria9chegeni@gmail.com>
2022-01-20 21:10:45 +0330

    use router and api config

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import api from './services/api';
+import VueRouter from 'vue-router'
+import router from './router/index.js'
+import './assets/design/css/main.css';
+
+
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
-
+api.init()
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
