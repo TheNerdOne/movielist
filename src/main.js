b29943fee88ad185e26ed8121450f69a85f6d7f8
thenerdone <poria9chegeni@gmail.com>
2022-01-21 16:40:07 +0330

    add bootstrap vue to whole prokect

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue'
 import api from './services/api';
 import VueRouter from 'vue-router'
 import router from './router/index.js'
+import BootstrapVue from 'bootstrap-vue'
+
+import './assets/design/scss/main.scss';
 
 
+
+Vue.use(BootstrapVue)
 Vue.use(VueRouter)
 
 Vue.config.productionTip = false
